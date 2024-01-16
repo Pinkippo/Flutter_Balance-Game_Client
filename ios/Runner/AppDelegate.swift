ce0b0e0b4362a94b4a163f97c14429bcf583aa2b
Pinkippo <hseungwan8@gmail.com>
2024-01-16 22:19:53 +0900

    feat(deeplink): 네이티브 메서드 채널 구현
    - IOS / AOS 딥링크 네이티브 메서드 채널 생성

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,18 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+  ) -> Bool {
+    if let scheme = url.scheme,
+        scheme.localizedCaseInsensitiveCompare("yangjataekil") == .orderedSame,
+        let view = window?.rootViewController as? FlutterViewController {
+        let channel = FlutterMethodChannel(name: "yangjataekil.detail/channel", binaryMessenger: view.binaryMessenger)
+        channel.invokeMethod("initialLink", arguments: url.absoluteString)
+    }
+    return false
+  }
 }
