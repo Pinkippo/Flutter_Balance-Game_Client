c2549cd673a230e042097da6e86e3d2f88e17a37
Pinkippo <hseungwan8@gmail.com>
2024-01-18 00:16:43 +0900

    feat(kakao): 카카오 공유하기 환경 설정
    - .env 추가 앱 키 보안
    - 카카오 SDK 환경 설정

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,23 @@ import 'package:flutter_balance_game_client/controller/login_controller.dart';
 import 'package:flutter_balance_game_client/data/repository/auth_repository.dart';
 import 'package:flutter_balance_game_client/route/app_pages.dart';
 import 'package:flutter_balance_game_client/theme/app_thene.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
+import 'package:kakao_flutter_sdk_share/kakao_flutter_sdk_share.dart';
 
 void main() async {
 
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  /// .env 파일 로드
+  await dotenv.load(fileName: 'assets/config/.env');
+
+  /// 카카오 SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: dotenv.env['NATIVE_APP_KEY'],
+    javaScriptAppKey: dotenv.env['JV_KEY'],
+  );
 
   /// 세로모드 고정
   await SystemChrome.setPreferredOrientations([
